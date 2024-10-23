a929408e1f44906f8923154d13cd45c024260717
Orozbek Askarov <Orozbek.Askarov@nttdata.com>
2024-10-23 01:34:10 -0700

    get token when user changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import ChatForm from './components/ChatForm/ChatForm';
 import Conversation from './components/Conversation/Conversation';
 import { ConversationProvider } from './contexts/ConversationContext';
-import { UserProvider } from './contexts/UserContext';
+import { UserProvider, useUser } from './contexts/UserContext';
 import getAuthToken from './api/auth';
 
 function App() {
   const [isSidebarVisible, setSidebarVisible] = useState(false);
-
-  console.log(getAuthToken());                                      
+  const { user } = useUser();
+  
+  useEffect(() => {
+    if (!user) {
+      getAuthToken();
+    }
+   }, [user]);
 
   return (
     <UserProvider>
       <ConversationProvider>
         <div
-          className={`flex relative w-screen h-screen bg-chat-white dark:bg-chat-dark dark:text-default-txt-dark ${
-            isSidebarVisible ? 'px-2' : 'px-5'
-          }`}
+          className={`flex relative w-screen h-screen bg-chat-white dark:bg-chat-dark dark:text-default-txt-dark ${isSidebarVisible ? 'px-2' : 'px-5'
+            }`}
         >
           {isSidebarVisible && (
             <Sidebar setIsSideBarVisible={setSidebarVisible} />
