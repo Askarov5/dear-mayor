a7448926e63fc2b7721a3034f933f72712e8830a
Orozbek Askarov <Orozbek.Askarov@nttdata.com>
2024-10-29 12:09:59 -0500

    server is separated

--- client/src/utils/api.ts
+++
@@ -1,9 +0,0 @@
-// use full url in production, relative url in development
-// dev uses proxy to avoid CORS issues
-const BASE_API_URL = import.meta.env.PROD
-  ? import.meta.env.VITE_API_BASE_URL
-  : '/api';
-
-export const getFullUrl = (endpoint: string): string => {
-  return `${BASE_API_URL}${endpoint}`;
-};
---
+++ index.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const authUrl = process.env.VITE_AUTH_API_URL;
+const clientId = process.env.VITE_AUTH_CLIENT_ID;
+const clientSecret = process.env.VITE_AUTH_CLIENT_SECRET;
+const scope = process.env.VITE_AUTH_SCOPE;
+const grant_type = process.env.VITE_AUTH_GRANT_TYPE;
+
+const app = express();
+
+const corsOptions = {
+    origin: "https://dear-mayor.vercel.app/", // This should match your front end's URL
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.get('/', async (req, res) => {
+    await res.send('WELCOME TO DEAR MAYOR MIDDLEWARE');
+});
+
+app.get('/api', async (req, res) => {
+    await res.send('WELCOME TO DEAR MAYOR MIDDLEWARE');
+});
+
+app.post('/oauth2/v2.0/token', async (req, res) => {
+
+    if (!authUrl || !clientId || !clientSecret || !scope) {
+        throw new Error('Missing required environment variables');
+    }
+
+    const response = await fetch(`${authUrl}/oauth2/v2.0/token`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            grant_type: grant_type,
+            client_id: clientId,
+            client_secret: clientSecret,
+            scope: scope,
+        }).toString(),
+    })
+
+    return await res.json(await response.json());
+});
+
+const PORT = process.env.PORT || 5000; // You can specify the port here
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
